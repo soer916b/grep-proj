9399665e9d895694b904826c2347794b616d7211
soer916b <soerenjacobsen0@gmail.com>
2026-03-04 13:30:58 +0100

    fixed error handling logic in file opening/closing.\Added test-directory.

--- src/grep.c
+++ src/grep.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-
-// phase 1: I/O
-// mål: programmet kan åbne en fil, læse den linje for linje, og printe dem igen.
+#include <errno.h>
 
 int main(int argc, char* argv[]) {
-    // vi skal have tre argumenter: programnavn, filnavn og pattern.
-    // Vi tjekker at argc == 3:
     if (argc != 3) {
         fprintf(stderr, "argc %d is invalid.\nUsage: <%s> <file> <pattern>\n", argc, argv[0]);
         return 1;
     }
     
-    FILE* f = fopen(argv[1], "r");
-    if (f == NULL) {
-        fprintf(stderr, "file <%s> does not exist.\n", argv[1]);
+    FILE* file;
+    if ((file= fopen(argv[1], "r")) == NULL) {
+        perror("Error in opening file");
         return 1;
-    } else {
-        printf("file openened succesfully!\n");
     }
-    fclose(f);
+    printf("file <%s> openened succesfully!\n", argv[1]);
+
+    // TO-DO: do something with file here.
+
+        if (fclose(file) != 0) {
+            perror("Error in closing file");
+            return 1;
+        }
+        printf("file <%s> closed succesfully!\n", argv[1]);
 
     return 0;
 }
