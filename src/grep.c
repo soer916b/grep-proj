2b32fb3c1a97ed8943edda32e978e6253928705f
Søren Jacobsen <soerenjacobsen0@gmail.com>
2026-03-10 03:27:30 +0100

    refactoring; moved file processing logic out of main()

--- src/grep.c
+++ src/grep.c
@@ -3,46 +3,57 @@
 #include <string.h>
 #include <stdbool.h>
 
+int process_file(bool mul_files, char* pattern, char* filename) {
+    FILE* fptr;
+    if ((fptr= fopen(filename, "r")) == NULL) {
+        perror("Error in opening file.");
+        return 1;
+    }
+
+    char buff[1024];
+    while(fgets(buff, sizeof(buff), fptr)) {
+        char* pattern_ptr = strstr(buff, pattern);
+        size_t len = strlen(buff);
+        if (pattern_ptr != NULL) {
+            if (!mul_files) {
+                fputs(buff, stdout);
+                if (!(buff[len - 1] == '\n')) {
+                    fputs("\n", stdout);
+                }
+            } else {
+                fputs(filename, stdout);
+                fputs(": ", stdout);
+                fputs(buff, stdout);
+                if (!(buff[len - 1] == '\n')) {
+                    fputs("\n", stdout);
+                }
+            }
+        }
+    }
+        if (fclose(fptr) != 0) {
+            perror("Error in closing file");
+            return 1;
+        }
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         fprintf(stderr, "argc %d is invalid.\nUsage: <%s> <pattern> <file>\n", argc, argv[0]);
         return 1;
     }
-    bool multiple_files = false;
-    if (argc > 3) {
-        multiple_files = true;
+
+    bool mul = true;
+    if (argc == 3) {
+        mul = false;
     }
+
     int i;
-    for (i = 2; i <= argc-1; i++) {
-        FILE* fptr;
-        if ((fptr= fopen(argv[i], "r")) == NULL) {
-            perror("Error in opening file.");
+    for (i = 2; i < argc; i++) { 
+        if (process_file(mul, argv[1],argv[i]) == 1) {
+            perror("Error in file processing.");
             return 1;
         }
-        char buff[1024];
-        while(fgets(buff, sizeof(buff), fptr)) {
-            char* pattern_ptr = strstr(buff, argv[1]);
-            size_t len = strlen(buff);
-            if (pattern_ptr != NULL) {
-                if (!multiple_files) {
-                    fputs(buff, stdout);
-                    if (strcmp(&buff[len-1], "\n") != 0) {
-                        fputs("\n", stdout);
-                    }
-                } else {
-                    fputs(argv[i], stdout);
-                    fputs(": ", stdout);
-                    fputs(buff, stdout);
-                    if (strcmp(&buff[len-1], "\n") != 0) {
-                        fputs("\n", stdout);
-                    }
-                }
-            }
-        }
-            if (fclose(fptr) != 0) {
-                perror("Error in closing file");
-                return 1;
-            }
     }
     return 0;
 }
